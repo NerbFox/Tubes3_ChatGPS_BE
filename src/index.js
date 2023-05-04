1334f5bd47fea77c0c2d8718475e10757f843bb6
Rava Maulana <ravamaulana14@gmail.com>
2023-05-04 14:14:08 +0700

    feat: added config

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ const {
   addSession,
   saveHistory,
 } = require("./controllers/responseController.js");
-require("dotenv").config();
+require("dotenv").config({ path: "./config/.env" });
 const cors = require("cors");
 const app = express();
 
@@ -25,12 +25,9 @@ app.post("/history", saveHistory);
 
 async function start() {
   try {
-    await mongoose.connect(
-      "mongodb+srv://test_user:test_user123@test-cluster.styaf4q.mongodb.net/chatgps-tubes-stima?retryWrites=true&w=majority",
-      {
-        connectTimeoutMS: 300000,
-      }
-    );
+    await mongoose.connect(process.env.MONGODB_URI, {
+      connectTimeoutMS: 300000,
+    });
     const port = process.env.PORT || 5000;
     app.listen(port, console.log(`listening on port ${port}`));
   } catch (err) {
